ba2c7e8f686ee5847d2c98ebb827eb6dc96a433a
mateus <mateusv@email.com>
2020-08-17 19:37:12 -0300

    major rebrand to tomuto

--- tomuto.py
+++ tomuto.py
@@ -13,15 +13,16 @@ def configWindow(root):
     x = (screen_width/2) - (300/2)
     y = (screen_height/2) - (100/2)
     root.geometry("300x100+" + str(int(x)) + "+" + str(int(y)))
-    root.title("openPomodoro")
+    root.title("tomuto")
     root.iconbitmap("icon.ico")
 
     # window text
 
-    text = tk.Label(text="Open Pomodoro Timer")
-    text.config(font=("Verdana", 12))
-    text.pack()
+    flavorText = tk.Label(text="tomuto: open source pomodoro")
+    flavorText.config(font=("Verdana", 12))
+    flavorText.pack()
 
+    return flavorText
 
 def prefixZero(seconds):
     # adds extra 0 before single-digit seconds
@@ -32,35 +33,37 @@ def prefixZero(seconds):
 
     return displaysecs
 
-def timerDisplay(minutes, seconds):
+def timerDisplay(minutes, seconds, flavorText):
     # updates the timer display every time it's called
     displaysecs = prefixZero(seconds)
     displaymins = prefixZero(minutes)
     timerText.config(text=displaymins + ":" + displaysecs)
     if not done:
-        start(minutes, seconds)
+        start(minutes, seconds, flavorText)
 
 
-def startCaller(minutes, seconds):
+def startCaller(minutes, seconds, flavorText):
     global done
     # this calls the start function, but first disables the start button after it has been pressed.
 
+    flavorText.config(text='running...')
     startButton.configure(state=tk.DISABLED)
     resetButton.configure(state=tk.NORMAL)
+
     done = False
-    start(minutes, seconds)
+    start(minutes, seconds, flavorText)
 
 
-def start(minutes, seconds):
+def start(minutes, seconds, flavorText):
     global done
     # start the countdown procedure after button press
 
-    minutes, seconds = countdown(minutes, seconds)
+    minutes, seconds = countdown(minutes, seconds, flavorText)
     if not done:
-        root.after(1000, timerDisplay, minutes, seconds) # calls for an update every second
+        root.after(1000, timerDisplay, minutes, seconds, flavorText) # calls for an update every second
 
 
-def countdown(minutes, seconds):
+def countdown(minutes, seconds, flavorText):
     global done
     # does the actual math behind the timer
 
@@ -74,16 +77,21 @@ def countdown(minutes, seconds):
             minutes -= 1
     else:
         done = True
+        flavorText.config(text="boop boop!")
 
     return minutes, seconds
 
 
-def reset():
+def reset(flavorText):
     global done
-    global startMinutes
     # activates upon press of the Reset button.
 
+    # flavor text:
+
+    flavorText.config(text="tomuto: open source pomodoro")
+
     # resetting values:
+
     done = True
     minutes = startMinutes
     seconds = 60
@@ -95,12 +103,15 @@ def reset():
 
 
 def timerDisplayResetter(minutes, seconds):
+    # resets the display to start minutes value when reset is pressed.
+
     timerText.config(text=str(minutes) + ":" + "00")
+
 
 #### MAIN ####:
 
 root = tk.Tk() # starting up the Tkinter window
-configWindow(root) # apply window settings
+flavorText = configWindow(root) # apply window settings
 
 # initial timer...:
 
@@ -116,9 +127,9 @@ timerText.pack()
 
 # config buttons:
 
-startButton = tk.Button(root, text="Start", command=(lambda:startCaller(minutes, seconds)))
+startButton = tk.Button(root, text="Start", command=(lambda:startCaller(minutes, seconds, flavorText)))
 #pauseButton = tk.Button(root, text="Pause", command=(lambda:pause()))               KEY UPCOMING FEATURE
-resetButton = tk.Button(root, text="Reset", command=(lambda:reset()))               #KEY WIP FEATURE
+resetButton = tk.Button(root, text="Reset", command=(lambda:reset(flavorText)))               #KEY WIP FEATURE
 resetButton.configure(state=tk.DISABLED)
 startButton.place(relx=0.5, rely=0.77, anchor="center")
 #pauseButton.place(relx=0.35, rely=0.77, anchor="center")
